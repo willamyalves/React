0fae8bdfa6b1576e278c5c73a21d41a5af228fd4
Willamy Alves Gomes <willamy9100@gmail.com>
2024-06-24 22:48:32 -0300

    .

---
+++ PROJECT-1/src/components/listFruits.jsx
@@ -0,0 +1,23 @@
+const listFruits = () => {
+  return (
+    [
+        {
+            id: 1,
+            name: "banana",
+            color: "yellow"
+        },
+        {
+            id: 2,
+            name: "apple",
+            color: "red"
+        },
+        {
+            id: 3,
+            name: "lemon",
+            color: "green"
+        }
+    ]
+)
+}
+
+export default listFruits
